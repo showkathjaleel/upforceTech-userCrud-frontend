0fc69dc227fa5420d4eb4157a70f7494b1ebcab8
showkathjaleel <109936187+showkathjaleel@users.noreply.github.com>
2023-06-11 16:25:58 +0530

    changed to uppforce

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,3 @@
 //  export const BASE_URL = "http://localhost:8080"
-//   export const BASE_URL = "https://upforcetech-inky.vercel.app"
- export const BASE_URL = "https://upforcee.vercel.app"
-// export const BASE_URL='https://upforceapi.vercel.app'
+//  export const BASE_URL = "https://upforcee.vercel.app"
+ export const BASE_URL='https://uppforce.vercel.app'
